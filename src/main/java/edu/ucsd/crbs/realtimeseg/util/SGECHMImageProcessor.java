4cc9eaad78d52653d9347145ebba8cc383286edc
Christopher Churas <churas@gmail.com>
2015-01-22 17:01:02 -0800

    initial revision

---
+++ src/main/java/edu/ucsd/crbs/realtimeseg/util/SGECHMImageProcessor.java
@@ -0,0 +1,142 @@
+/*
+ * COPYRIGHT AND LICENSE
+ * 
+ * Copyright 2015 The Regents of the University of California All Rights Reserved
+ * 
+ * Permission to copy, modify and distribute any part of this realtime-segmentation for 
+ * educational, research and non-profit purposes, without fee, and without a 
+ * written agreement is hereby granted, provided that the above copyright notice, 
+ * this paragraph and the following three paragraphs appear in all copies.
+ * 
+ * Those desiring to incorporate this realtime-segmentation into commercial products
+ * or use for commercial purposes should contact the Technology Transfer Office, 
+ * University of California, San Diego, 9500 Gilman Drive, Mail Code 0910, 
+ * La Jolla, CA 92093-0910, Ph: (858) 534-5815, FAX: (858) 534-7345, 
+ * E-MAIL:invent@ucsd.edu.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR 
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING 
+ * LOST PROFITS, ARISING OUT OF THE USE OF THIS realtime-segmentation, EVEN IF THE UNIVERSITY 
+ * OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * THE realtime-segmentation PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND THE UNIVERSITY 
+ * OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, 
+ * ENHANCEMENTS, OR MODIFICATIONS. THE UNIVERSITY OF CALIFORNIA MAKES NO 
+ * REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER IMPLIED OR 
+ * EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR THAT THE USE OF 
+ * THE realtime-segmentation WILL NOT INFRINGE ANY PATENT, TRADEMARK OR OTHER RIGHTS. 
+ */
+
+package edu.ucsd.crbs.realtimeseg.util;
+
+import edu.ucsd.crbs.realtimeseg.App;
+import edu.ucsd.crbs.realtimeseg.io.ResourceToFile;
+import edu.ucsd.crbs.realtimeseg.io.ResourceToFileImpl;
+import edu.ucsd.crbs.realtimeseg.io.StringReplacer;
+import edu.ucsd.crbs.realtimeseg.job.CHMCommandLineJobViaSGE;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Christopher Churas <churas@ncmir.ucsd.edu>
+ */
+public class SGECHMImageProcessor implements ImageProcessor,StringReplacer {
+    
+    public static final String OUTPUT_DIR_TOKEN = "@@OUTPUT_DIR@@";
+    public static final String CHM_BINARY_TOKEN = "@@CHM_BINARY@@";
+    public static final String TILE_SIZE_TOKEN = "@@TILE_SIZE@@";
+    public static final String OVERLAP_TOKEN = "@@OVERLAP@@";
+    public static final String TRAINED_MODEL_TOKEN = "@@TRAINED_MODEL@@";
+    public static final String MATLAB_DIR_TOKEN = "@@MATLAB_DIR@@";
+    public static final String CONVERT_BINARY_TOKEN = "@@CONVERT_BINARY@@";
+    public static final String COLORS_TO_ZERO_OUT_TOKEN = "@@COLORS_TO_ZERO_OUT@@";
+    
+    
+    private static final Logger _log = Logger.getLogger(SGECHMImageProcessor.class.getName());
+    
+    private String _inputImageDir;
+    private String _workingDir;
+    private String _trainedModel;
+    private String _binary;
+    private String _matlabDir;
+    private String _colorsToZeroOut;
+    private String _tileSize;
+    private String _script;
+    private String _queue;
+    private String _convert;
+    
+     public SGECHMImageProcessor(final String inputImageDir,
+            final String workingDir,final String trainedModel,
+            final String binary,final String matlabDir,final String colorsToZeroOut,
+            final String tileSize,
+            final String queue,
+            final String convert){
+        _inputImageDir = inputImageDir;
+        _workingDir = workingDir;
+        _trainedModel = trainedModel;
+        _binary = binary;
+        _matlabDir = matlabDir;
+        _colorsToZeroOut = colorsToZeroOut;
+        _tileSize = tileSize+"x"+tileSize;
+        _queue = queue;
+        _convert = convert;
+        _log.log(Level.INFO,"Image Processor colors to zero out: {0}",_colorsToZeroOut);
+        createCommandLineScript();
+    }
+
+    @Override
+    public String replace(String line) {
+        if (line == null){
+            return null;
+        }
+        return line.replaceAll(OUTPUT_DIR_TOKEN, _workingDir)
+                   .replaceAll(CHM_BINARY_TOKEN, _binary)
+                   .replaceAll(TILE_SIZE_TOKEN, _tileSize)
+                   .replaceAll(OVERLAP_TOKEN, "0x0")
+                   .replaceAll(TRAINED_MODEL_TOKEN, _trainedModel)
+                   .replaceAll(MATLAB_DIR_TOKEN, _matlabDir)
+                   .replaceAll(CONVERT_BINARY_TOKEN, _convert)
+                   .replaceAll(COLORS_TO_ZERO_OUT_TOKEN, _colorsToZeroOut);
+    }
+    
+    private void createCommandLineScript()  {
+        try {
+            ResourceToFile scriptWriter = new ResourceToFileImpl();
+            _script = _workingDir + File.separator + "chmviasge.sh";
+            scriptWriter.writeResourceToScript("/chmviasge.sh.template", _script, 
+                    this);
+            File scriptFile = new File(_script);
+            scriptFile.setExecutable(true);
+        }
+        catch(Exception ex){
+            _log.log(Level.WARNING,
+                    "Caught exception trying to create {0} file : {1}",
+                    new Object[]{_script,ex.getMessage()});
+        }
+    }
+    
+     
+    @Override
+    public void process(String image) {
+        File checkForFile = new File(_inputImageDir+File.separator+image);
+        if (checkForFile.exists() == false){
+            return;
+        }
+        CHMCommandLineJobViaSGE job = new CHMCommandLineJobViaSGE(_script,image,
+                checkForFile.getAbsolutePath(),_workingDir,_queue);
+        
+        _log.log(Level.INFO,"Submitting image {0} for processing and writing output to {1}",
+                new Object[]{image,_workingDir});
+        
+        App.tilesToProcess.add(job);
+        
+    }
+
+
+    
+    
+    
+}
