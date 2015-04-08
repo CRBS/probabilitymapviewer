1baf9b444d8a407cf4c18f3abc2c053e57a64df1
Christopher Churas <churas@gmail.com>
2015-04-07 18:45:43 -0700

    re indented and added collectionname, and sliceaquiretime

--- src/main/java/edu/ucsd/crbs/segmenter/handler/StatusHandler.java
+++ src/main/java/edu/ucsd/crbs/segmenter/handler/StatusHandler.java
@@ -27,7 +27,6 @@
  * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR THAT THE USE OF 
  * THE segmenter WILL NOT INFRINGE ANY PATENT, TRADEMARK OR OTHER RIGHTS. 
  */
-
 package edu.ucsd.crbs.segmenter.handler;
 
 import edu.ucsd.crbs.segmenter.App;
@@ -44,98 +43,112 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
  *
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
-public class StatusHandler extends AbstractHandler  {
+public class StatusHandler extends AbstractHandler {
 
     private int _numCores;
-    
-    public StatusHandler(int numCores){
+
+    public StatusHandler(int numCores) {
         _numCores = numCores;
-        if (_numCores <= 0){
+        if (_numCores <= 0) {
             _numCores = 1;
         }
     }
-    
+
     @Override
-    public void handle(String string, Request request, 
-            HttpServletRequest servletRequest, 
-            HttpServletResponse servletResponse) throws IOException, 
+    public void handle(String string, Request request,
+            HttpServletRequest servletRequest,
+            HttpServletResponse servletResponse) throws IOException,
             ServletException {
-   
-            int tilesToProcess = App.tilesToProcess.size();
-            String avgTimePerTile = "?";
-            double rawAvgTimePerTile = 0.0;
-            String totalTimeHours = "?";
-            String estTimeMinutes = "?";
-            String estCpuHours = "?";
-            String numCoresInUse = "?";
-            int numCoresInUseCount = 0;
-            
-            for (Future f : App.futureTaskList ){
-                
-                if (!f.isCancelled() && !f.isDone()){
-                    numCoresInUseCount++;
-                }
-                
+
+        int tilesToProcess = App.tilesToProcess.size();
+        String avgTimePerTile = "?";
+        double rawAvgTimePerTile = 0.0;
+        String totalTimeHours = "?";
+        String estTimeMinutes = "?";
+        String estCpuHours = "?";
+        String numCoresInUse = "?";
+        int numCoresInUseCount = 0;
+
+        for (Future f : App.futureTaskList) {
+
+            if (!f.isCancelled() && !f.isDone()) {
+                numCoresInUseCount++;
             }
-            // @TODO this is stupid, but it will be less confusing to the user
-            if (numCoresInUseCount > _numCores){
-                numCoresInUseCount = _numCores;
-            }
-            numCoresInUse = Integer.toString(numCoresInUseCount);
-            
-            if (App.totalJobsRun > 0){
-                long totalTime = App.totalRunTimeOfJobs;
-                long numJobs = App.totalJobsRun;
-                
-                if (numJobs > 0){
-                    rawAvgTimePerTile = (double)totalTime/(double)numJobs;
-                    avgTimePerTile = Long.toString(Math.round(rawAvgTimePerTile/(double)1000.0));
-                    totalTimeHours = Long.toString(Math.round((double)totalTime/(double)1000.0/(double)3600.0));
-                    if (tilesToProcess > 0){
+
+        }
+        // @TODO this is stupid, but it will be less confusing to the user
+        if (numCoresInUseCount > _numCores) {
+            numCoresInUseCount = _numCores;
+        }
+        numCoresInUse = Integer.toString(numCoresInUseCount);
+
+        if (App.totalJobsRun > 0) {
+            long totalTime = App.totalRunTimeOfJobs;
+            long numJobs = App.totalJobsRun;
+
+            if (numJobs > 0) {
+                rawAvgTimePerTile = (double) totalTime / (double) numJobs;
+                avgTimePerTile = Long.toString(Math.round(rawAvgTimePerTile / (double) 1000.0));
+                totalTimeHours = Long.toString(Math.round((double) totalTime / (double) 1000.0 / (double) 3600.0));
+                if (tilesToProcess > 0) {
                         //take # of tiles to process and divide by num cores 
-                        //and multiply by average time per tile then convert to
-                        //minutes
-                        estTimeMinutes  = Long.toString(Math.round(tilesToProcess/(double)_numCores*rawAvgTimePerTile/(double)1000.0/(double)60.0));
-                        
+                    //and multiply by average time per tile then convert to
+                    //minutes
+                    estTimeMinutes = Long.toString(Math.round(tilesToProcess / (double) _numCores * rawAvgTimePerTile / (double) 1000.0 / (double) 60.0));
+
                         //take # of tiles to process and multiply by avg time per tile
-                        //and convert to hours
-                        estCpuHours = Long.toString(Math.round(tilesToProcess*rawAvgTimePerTile/(double)1000.0/(double)3600.0));
-                    }
+                    //and convert to hours
+                    estCpuHours = Long.toString(Math.round(tilesToProcess * rawAvgTimePerTile / (double) 1000.0 / (double) 3600.0));
                 }
-                
             }
-            StringBuilder sb = new StringBuilder();
-            sb.append("{ \"tilestoprocess\": ");
-            sb.append(tilesToProcess);
-            sb.append(",\"futuretasklistsize\": ");
-            sb.append(App.futureTaskList.size());
-            sb.append(",\"esttimehours\": \"");
-            sb.append(estTimeMinutes);
-            sb.append("\"");
-            sb.append(",\"estcpuhours\": \"");
-            sb.append(estCpuHours);
-            sb.append("\"");
-            sb.append(",\"tilesprocessed\": ");
-                    sb.append(App.totalProcessedCount);
-                    sb.append(",\"cpuconsumedhours\": \"");
-                    sb.append(totalTimeHours);
-                    sb.append("\"");
-                    sb.append(",\"numcoresinuse\": \"");
-                    sb.append(numCoresInUse);
-                    sb.append("\"");
-                    sb.append(",\"avgtimepertile\": \"");
-                    sb.append(avgTimePerTile);
-                    sb.append("\"");
-                    sb.append(",\"latestslice\": \"");
-                    sb.append(App.latestSlice);
-                    sb.append("\"}");
-            
-            servletResponse.setContentType("application/json");
-            servletResponse.setCharacterEncoding("UTF-8");
-            servletResponse.getWriter().write(sb.toString());
-            servletResponse.setStatus(HttpServletResponse.SC_OK);
-            request.setHandled(true);
+
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("{ \"tilestoprocess\": ");
+        sb.append(tilesToProcess);
+        sb.append(",\"futuretasklistsize\": ");
+        sb.append(App.futureTaskList.size());
+        sb.append(",\"esttimehours\": \"");
+        sb.append(estTimeMinutes);
+        sb.append("\"");
+        sb.append(",\"estcpuhours\": \"");
+        sb.append(estCpuHours);
+        sb.append("\"");
+        sb.append(",\"tilesprocessed\": ");
+        sb.append(App.totalProcessedCount);
+
+        sb.append(",\"cpuconsumedhours\": \"");
+        sb.append(totalTimeHours);
+        sb.append("\"");
+
+        sb.append(",\"numcoresinuse\": \"");
+        sb.append(numCoresInUse);
+        sb.append("\"");
+
+        sb.append(",\"avgtimepertile\": \"");
+        sb.append(avgTimePerTile);
+        sb.append("\"");
+
+        sb.append(",\"latestslice\": \"");
+        sb.append(App.latestSlice);
+        sb.append("\"");
+
+        sb.append(",\"collectionname\": \"");
+        sb.append(App.collectionName);
+        sb.append("\"");
+
+        sb.append(",\"sliceaquiretime\": \"");
+        sb.append(App.sliceAquireTime);
+        sb.append("\"");
+
+        
+        sb.append("}");
+
+        servletResponse.setContentType("application/json");
+        servletResponse.setCharacterEncoding("UTF-8");
+        servletResponse.getWriter().write(sb.toString());
+        servletResponse.setStatus(HttpServletResponse.SC_OK);
+        request.setHandled(true);
     }
 
-    
 }
