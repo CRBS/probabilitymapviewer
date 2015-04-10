884885381162747df2637566768dbd4d9067568c
Chris Churas <churas@gmail.com>
2015-04-10 08:40:38 -0700

    added javadoc

--- src/main/java/edu/ucsd/crbs/segmenter/io/WorkingDirCreator.java
+++ src/main/java/edu/ucsd/crbs/segmenter/io/WorkingDirCreator.java
@@ -34,9 +34,16 @@ import java.io.File;
 import java.util.Properties;
 
 /**
+ * Defines method to create working directory
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
 public interface WorkingDirCreator {
 
+    /**
+     * Creates working directory based on values in <b>props</b> parameter
+     * @param props
+     * @return Created working directory
+     * @throws Exception 
+     */
     public File createWorkingDir(Properties props) throws Exception;
 }
--- src/main/java/edu/ucsd/crbs/segmenter/io/WorkingDirCreatorImpl.java
+++ src/main/java/edu/ucsd/crbs/segmenter/io/WorkingDirCreatorImpl.java
@@ -38,7 +38,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Creates Working directory
  * @author Christopher Churas <churas@ncmir.ucsd.edu>
  */
 public class WorkingDirCreatorImpl implements WorkingDirCreator{
@@ -46,6 +46,16 @@ public class WorkingDirCreatorImpl implements WorkingDirCreator{
         private static final Logger _log = Logger.getLogger(WorkingDirCreatorImpl.class.getName());
 
     
+    /**
+     * Examines <b>props</b> looking for {@value #DIR_ARG} property, if found
+     * method checks if directory exists, if it does not the code attempts to
+     * create it and if successful the <b>props</b> has a new property 
+     * {@value #TEMP_DIR_CREATED_FLAG} is added and set to <b>true</b>
+     * @param props
+     * @return Working Directory even if method did not create it
+     * @throws Exception If <b>props</b> is null or {@value #DIR_ARG} is not set
+     *  or if there is a problem creating the directory
+     */
     @Override
     public File createWorkingDir(Properties props) throws Exception {
         if (props == null){
