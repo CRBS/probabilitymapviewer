6b360c8b61294d5bb2113b9316e9089fe0129b7e
Chris Churas <churas@gmail.com>
2016-04-20 17:02:05 -0700

    added unit tests and reformatted code

--- src/test/java/edu/ucsd/crbs/segmenter/slice/TestDm4SliceConverterDaemon.java
+++ src/test/java/edu/ucsd/crbs/segmenter/slice/TestDm4SliceConverterDaemon.java
@@ -6,6 +6,7 @@
 package edu.ucsd.crbs.segmenter.slice;
 
 import edu.ucsd.crbs.segmenter.App;
+import java.util.Properties;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -47,5 +48,54 @@ public class TestDm4SliceConverterDaemon {
             assertTrue(npe.getMessage().equals("props is null"));
         }
     }
+    
+    @Test
+    public void testConstructorInputImageArgIsNull(){
+        try {
+            Properties props = new Properties();
+            new Dm4SliceConverterDaemon(props, null);
+            fail("Expected exception");
+        } catch(NullPointerException npe){
+            assertTrue(npe.getMessage(),
+                    npe.getMessage().equals(App.INPUT_IMAGE_ARG 
+                    + " property is null"));
+        }
+    }
+    
+    @Test
+    public void testConstructorAdjustedInputImageArgIsNull(){
+        try {
+            Properties props = new Properties();
+            props.setProperty(App.INPUT_IMAGE_ARG,"");
+            
+            new Dm4SliceConverterDaemon(props, null);
+            fail("Expected exception");
+        } catch(NullPointerException npe){
+            assertTrue(npe.getMessage(),
+                    npe.getMessage().equals(App.ADJUSTED_INPUT_IMAGE_ARG 
+                    + " property is null"));
+        }
+    }
+    
+    @Test
+    public void testGetDestinationDirectory(){
+            Properties props = new Properties();
+            props.setProperty(App.INPUT_IMAGE_ARG,"");
+            props.setProperty(App.ADJUSTED_INPUT_IMAGE_ARG,"hello");
+            Dm4SliceConverterDaemon daemon = 
+                    new Dm4SliceConverterDaemon(props, null);
+            assertTrue(daemon.getDestinationDirectory().equals("hello"));
+    }
+    
+    // test getSecondYoungestDm4File No files
+    
+    // test getSecondYoungestDm4File one file
+    
+    //test getSecondYoungestDm4File two files
+    
+    //test getSecondYoungestDm4File three files
+    
+    //test getSecondYoungestDm4File four files
+    
     
 }
