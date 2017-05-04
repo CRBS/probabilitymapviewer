e88c2fd8ffdf28d01bd0518188017fa5c9dd7909
Chris Churas <churas@gmail.com>
2017-05-04 09:24:22 -0700

    removed unused chm and ilastik specific calls

--- src/main/java/edu/ucsd/crbs/probabilitymapviewer/processor/ImageProcessorFactory.java
+++ src/main/java/edu/ucsd/crbs/probabilitymapviewer/processor/ImageProcessorFactory.java
@@ -71,56 +71,6 @@ public class ImageProcessorFactory {
                 getAnalyzingTile(layer));
     }
     
-    private ImageProcessor getSGECHMImageProcessor(final String workingDir,
-            CustomLayer layer){
-        
-        return new SGECHMImageProcessor(_props.getProperty(App.ADJUSTED_INPUT_IMAGE_ARG),
-                    workingDir,
-                    layer.getScript(),
-                    _props.getProperty(App.CHM_BIN_ARG)+File.separator+CHM_TEST_SH,
-                    _props.getProperty(App.MATLAB_ARG),layer.getConvertColor(),
-                    _props.getProperty(App.TILE_SIZE_ARG),
-                    _props.getProperty(App.SGE_CHM_QUEUE_ARG),
-                    _props.getProperty(App.CONVERT_ARG),
-                    getAnalyzingTile(layer));
-    }
-    
-    private ImageProcessor getSimpleCHMImageProcessor(final String workingDir,
-            CustomLayer layer){
-        return new SimpleCHMImageProcessor(_props.getProperty(App.ADJUSTED_INPUT_IMAGE_ARG),
-                    workingDir,
-                    layer.getScript(),
-                    _props.getProperty(App.CHM_BIN_ARG)+File.separator+CHM_TEST_SH,
-                    _props.getProperty(App.MATLAB_ARG),layer.getConvertColor(),
-                    _props.getProperty(App.TILE_SIZE_ARG),
-                    getAnalyzingTile(layer));
-    }
-    
-    private ImageProcessor getSimpleIlastikImageProcessor(final String workingDir,
-            CustomLayer layer){
-        /** @TODO Add support for analyzing tile */
-        return new SimpleIlastikImageProcessor(_props.getProperty(App.ADJUSTED_INPUT_IMAGE_ARG),
-                    workingDir,
-                    layer.getScript(),
-                    _props.getProperty(App.ILASTIK_ARG)+File.separator+RUN_ILASTIK_SH,
-                    _props.getProperty(App.MATLAB_ARG),layer.getConvertColor(),
-                    _props.getProperty(App.TILE_SIZE_ARG));
-        
-        
-    }
-    
-    private ImageProcessor getSGEIlastikImageProcessor(final String workingDir,
-            CustomLayer layer){
-        /** @TODO Add support for analyzing tile */
-        return new SGEIlastikImageProcessor(_props.getProperty(App.ADJUSTED_INPUT_IMAGE_ARG),
-                    workingDir,
-                    layer.getScript(),
-                    _props.getProperty(App.ILASTIK_ARG)+File.separator+RUN_ILASTIK_SH,
-                    layer.getConvertColor(),
-                    _props.getProperty(App.SGE_ILASTIK_QUEUE_ARG),
-                    _props.getProperty(App.CONVERT_ARG));
-    }
-    
     private String getAnalyzingTile(CustomLayer layer){
         String analyzingTile = null;
         if (_props.getProperty(App.DISABLE_ANALYZING_TILE_ARG,"true").equals("false")){
