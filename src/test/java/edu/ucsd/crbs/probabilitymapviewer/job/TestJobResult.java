55fb43cca2cb0388a6e209b13bdbb09b461c680f
Chris Churas <churas@gmail.com>
2017-06-19 15:33:43 -0700

    added tests

---
+++ src/test/java/edu/ucsd/crbs/probabilitymapviewer/job/TestJobResult.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ucsd.crbs.probabilitymapviewer.job;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author churas
+ */
+public class TestJobResult {
+    
+    public TestJobResult() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testGetterAndSetter(){
+        JobResult jr = new JobResult();
+        assertTrue(jr.getRunTimeInMilliseconds() == 0);
+        jr.setRunTimeInMilliseconds(1L);
+        assertTrue(jr.getRunTimeInMilliseconds() == 1L);
+        
+    }
+
+    
+}
